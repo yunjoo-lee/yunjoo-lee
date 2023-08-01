b0d9d7fcb960fb3f87516b08bd1e4b5ba633671b
yunjoo.lee <yunjoo.lee@dabeeo.com>
2023-08-01 14:42:06 +0900

    feat: 회전, 크기 조절 값 배열로 계산

--- rotateAndResize/showChangedValues.js
+++ rotateAndResize/showChangedValues.js
@@ -6,19 +6,29 @@ setHandleStyle();
 // let startRadius = 10;
 let previousLength = vectorLayer.length;
 
-let rotating = 0;
-let rotated = 0;
-let scaled = [1, 1];
-let scaling = [1, 1];
+var rotateArray = [0];
+var scaleArray = [[1, 1]];
+
+let rotatedElement = 0;
+let scaledElement = [1, 1];
+
+const sumArrayElements = (arr) => {
+  return arr.reduce((sum, current) => sum + current, 0);
+};
+
+const multiplyArrayElements = (arr) => {
+  return arr.reduce(
+    (product, current) => [product[0] * current[0], product[1] * current[1]],
+    [1, 1]
+  );
+};
 
 // Handle rotate on first point
 const firstPoint = false;
 interaction.on(["select"], (e) => {
   if (vectorLayer.length > previousLength) {
-    rotating = 0;
-    rotated = 0;
-    scaled = [1, 1];
-    scaling = [1, 1];
+    rotateArray = [0];
+    scaleArray = [[1, 1]];
     // 이전 배열의 길이 업데이트
     previousLength = vectorLayer.length;
   }
@@ -37,25 +47,27 @@ interaction.on(["rotatestart", "translatestart", "scalestart"], (e) => {
   // // Translation: 이동 거리를 구해야할 때 이 옵션 주석 처리 해제
   // d=[0,0];
 });
+
 interaction.on("rotating", (e) => {
-  rotating = (((e.angle * 180) / Math.PI - 180) % 360) + 180 || 0;
-  $("#rotateinfo").text("rotate: " + (rotated + rotating).toFixed(2));
+  rotatedElement = (((e.angle * 180) / Math.PI - 180) % 360) + 180;
+  console.log(rotatedElement);
+  $("#rotateinfo").text(
+    "rotate: " + (sumArrayElements(rotateArray) + rotatedElement).toFixed(2)
+  );
+  // console.log(sumArrayElements(rotateArray));
   // Set angle attribute to be used on style !
   // e.feature.set("angle", startangle - e.angle);
-});
-interaction.on(["rotateend", "translateend", "scaleend"], (e) => {
-  rotated = rotating + rotated;
-  scaled = [scaling[0] * scaled[0], scaling[1] * scaled[1]];
 });
 
 // "scale: " + e.scale[0].toFixed(2) + "," + e.scale[1].toFixed(2)
 interaction.on("scaling", (e) => {
-  scaling = [e.scale[0], e.scale[1]];
+  scaledElement = [e.scale[0], e.scale[1]];
+  console.log(scaledElement);
   $("#scaleinfo").text(
     "scale: " +
-      (scaling[0] * scaled[0]).toFixed(2) +
-      "," +
-      (scaling[1] * scaled[1]).toFixed(2)
+      (multiplyArrayElements(scaleArray)[0] * scaledElement[0]).toFixed(2) +
+      " , " +
+      (multiplyArrayElements(scaleArray)[1] * scaledElement[1]).toFixed(2)
   );
   if (firstPoint) {
     interaction.setCenter(
@@ -66,4 +78,11 @@ interaction.on("scaling", (e) => {
   //   const feature = e.features.item(0);
   // feature.set("radius", startRadius * Math.abs(e.scale[0]));
   // }
+});
+
+interaction.on(["rotateend", "translateend", "scaleend"], (e) => {
+  rotateArray.push(rotatedElement);
+  // scaled = [scaling[0] * scaled[0], scaling[1] * scaled[1]];
+  scaleArray.push(scaledElement);
+  console.log(scaleArray);
 });
