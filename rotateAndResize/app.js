e22968bc2d8136998fa51cfbe304fba24b8a5849
yunjoo.lee <yunjoo.lee@dabeeo.com>
2023-08-14 08:22:08 +0900

    feat: 노드로 서버 띄워서 georeferencing 페이지 실행 npm run start 명령어로 실행

--- rotateAndResize/app.js
+++ rotateAndResize/app.js
@@ -1,26 +1,22 @@
 const express = require("express");
-// const morgan = require("morgan");
 const path = require("path");
+const morgan = require("morgan");
 
-// dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-// app.use(morgan("dev"));
-// app.use('/', express.static(path.join(__dirname, 'css폴더이름')));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "/")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get(
   "/",
-  (req, res, next) => {
-    console.log("GET / 요청에서만 실행됩니다.");
-    next();
-  },
-  (req, res) => {
-    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
-  }
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+  })
 );
+
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send(err.message);
